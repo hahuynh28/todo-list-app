2fcbd2ef75b38c91ce7506a5c4cf76b22620734e
hahuynh28 <huynhh733@gmail.com>
2026-01-06 18:05:46 -0500

    Added comments in ThemeContext file

--- frontend/src/context/ThemeContext.jsx
+++ frontend/src/context/ThemeContext.jsx
@@ -2,26 +2,36 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const ThemeContext = createContext();
 
+// Create a context to share the theme across the entire app
 export function ThemeProvider({ children }) {
+  // State theme
+  // Get the value from localStorage if available, otherwise the default is "light"
   const [theme, setTheme] = useState(() => {
     return localStorage.getItem("theme") || "light";
   });
 
+  // Run whenever the theme changes
   useEffect(() => {
     const root = document.documentElement;
+
     if (theme === "dark") {
+      // If it's dark mode, add the class "dark"
       root.classList.add("dark");
     } else {
+      // If it's light mode, delete the "dark" class.
       root.classList.remove("dark");
     }
+    // Save the theme to localStorage
     localStorage.setItem("theme", theme);
   }, [theme]);
 
+  // Theme switching function
   const toggleTheme = () => {
     setTheme((prev) => (prev === "dark" ? "light" : "dark"));
   };
 
   return (
+    // Provide theme and toggleTheme for all child components
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
